b4b3165ca29d321c513776b95a21d1d617c696ba
Rwan Abdelmonem <rabdelmonem0@gmail.com>
2021-06-23 10:15:28 -0700

    added db.js

---
+++ Develop/public/serviceWorker.js
@@ -0,0 +1,12 @@
+// items to cache
+
+// install the service worker
+self.addEventListener('install', e => {
+
+});
+
+// cache all the get requests and activate service worker
+self.addEventListener('fetch', e => {
+    // check if there's an api route
+
+});
