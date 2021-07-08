0fc645aa17b4fb9046422dd2a8e680911cc44e49
Rwan Abdelmonem <rabdelmonem0@gmail.com>
2021-07-07 17:18:30 -0700

    moved everything out of the devlop folder

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
   "/db.js",
   "/index.html",
   "/index.js",
-  "/manifest.webmanifest",
+  "/manifest.json",
   "/style.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
--- server.js
+++ server.js
@@ -15,10 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-}).then(() => console.log('connected to db').catch(err => console.log('not connected! ', err)));
+// connect to mongodb atlas cluster
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+).then(() => console.log('connected to db').catch(err => console.log('not connected! ', err)));
 
 // routes
 app.use(require("./routes/api.js"));
